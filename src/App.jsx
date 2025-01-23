3f1c5cdbfec4c8a3c4c07fe5b6592daeddfcd27a
Sender60 <butkov60@gmail.com>
2025-01-23 19:19:50 +0300

    Создание пустого проекта, включая eslint husky

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/image/logo.svg" alt="logo" />
+      </header>
+      <section className="App-content" />
+    </div>
+  );
+}
+
+export default App;
